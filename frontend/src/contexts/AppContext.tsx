2f5b84009c048f597fe8e2f728a826e4e07d10b8
Pedro <ferrarezzolabs@outlook.com>
2026-04-12 18:55:18 -0300

    refactor: move create websocket connection useEffect to AppContext component

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,5 +1,8 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import { createContext, useContext, useMemo, useRef, useState } from 'react';
 import type { ServerConnectionStatus } from "../types/server";
+import { createWebSocket } from "../lib/websocket";
+import { useEffect } from 'react';
+import { toast } from 'sonner';
 
 /**Tipo do Contexto da Aplicação */
 type AppContextType = {
@@ -7,6 +10,7 @@ type AppContextType = {
     setRagServerConnected: React.Dispatch<React.SetStateAction<ServerConnectionStatus>>;
     isLoading: boolean;
     setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    ws: React.RefObject<WebSocket | null>;
 }
 
 /**Contexto da Aplicação */
@@ -16,14 +20,61 @@ const AppContext = createContext<AppContextType | undefined>(undefined);
 export function AppContextProvider({ children }: { children: React.ReactNode }) {
   const [ragServerConnected, setRagServerConnected] = useState<ServerConnectionStatus>("disconnected");
   const [isLoading, setIsLoading] = useState(false);
+  const ws = useRef<WebSocket | null>(null);
 
   // Memoiza os valores do contexto para evitar re-renderizações desnecessárias nos componentes consumidores
   const values = useMemo(() => ({
         ragServerConnected,
         setRagServerConnected,
         isLoading,
-        setIsLoading
+        setIsLoading,
+        ws
     }), [ragServerConnected, isLoading]);
+
+  useEffect(() => {
+    ws.current = createWebSocket({ 
+        setRagServerConnected, 
+        onOpenCallback: () => {
+            toast.info("Conexão estabelecida com o servidor de RAG");
+        },
+        onCloseCallback: (event) => {
+            const { code, wasClean } = event;
+
+            const codeMessages: Record<number, string> = {
+                1000: "Conexão encerrada com o servidor de RAG",
+                1001: "Servidor de RAG indisponível ou navegação interrompida",
+                1002: "Erro de protocolo durante conexão com o servidor de RAG",
+                1003: "Tipo de dado não suportado pelo servidor de RAG",
+                1006: "Conexão interrompida com o servidor de RAG",
+                1007: "Dados inválidos recebidos do servidor de RAG",
+                1008: "Violação de política do servidor de RAG",
+                1009: "Mensagem muito grande recebida do servidor de RAG",
+                1011: "Erro interno do servidor de RAG",
+            };
+
+            if (codeMessages[code]) {
+                toast.info(codeMessages[code]);
+                return;
+            }
+
+            if (!wasClean) {
+                toast.info(`Conexão encerrada inesperadamente com o servidor de RAG: (${code})`);
+                return;
+            }
+
+            toast.info("Conexão encerrada inesperadamente com o servidor de RAG");
+        },
+        errorCallback: (err) => {
+            toast.error(`Erro inesperado durante estabelecimento da conexão com o servidor de RAG: ${err}`);
+        }
+    });
+
+    return () => {
+        ws.current?.close();
+        ws.current = null;
+    }
+
+  }, [])
 
   return (
     <AppContext.Provider value={values}>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,61 +3,25 @@ import { useState, useRef, useEffect, useMemo } from "react";
 import QuestionCard from "../components/QuestionCard";
 import type { LmmResponse, RagQuestion } from "../types/rag";
 import AnswerCard from "../components/AnswerCard";
-import { configureHandleError, configureHandleMessage, createWebSocket, sendQuestion } from "../lib/websocket";
+import { configureHandleError, configureHandleMessage, sendQuestion } from "../lib/websocket";
 import Header from "@/components/Header";
 import { useAppContext } from "@/contexts/AppContext";
 import { toast } from "sonner";
 
 /**Página Principal */
 export default function Home(){
-    const { setRagServerConnected } = useAppContext();
+    const { ws } = useAppContext();
     const [question, setQuestion] = useState("");
     const [chatInProgress, setChatInProgress] = useState(false);
     const [ragQuestions, setRagQuestions] = useState<RagQuestion[]>([]);
     const [lmmResponses, setLmmResponses] = useState<LmmResponse[]>([]);
     const ragConversationsScrollRef = useRef<HTMLDivElement>(null);
-    const ws = useRef<WebSocket | null>(null);
     const lastQuestion = useMemo(() => {
         return  ragQuestions[ragQuestions.length - 1];
     }, [ragQuestions]);
 
     useEffect(() => {
-        ws.current = createWebSocket({ 
-            setRagServerConnected, 
-            onOpenCallback: () => {
-                toast.info("Conexão estabelecida com o servidor de RAG");
-            },
-            onCloseCallback: (event) => {
-                const { code, wasClean } = event;
-
-                const codeMessages: Record<number, string> = {
-                    1000: "Conexão encerrada com o servidor de RAG",
-                    1001: "Servidor de RAG indisponível ou navegação interrompida",
-                    1002: "Erro de protocolo durante conexão com o servidor de RAG",
-                    1003: "Tipo de dado não suportado pelo servidor de RAG",
-                    1006: "Conexão interrompida com o servidor de RAG",
-                    1007: "Dados inválidos recebidos do servidor de RAG",
-                    1008: "Violação de política do servidor de RAG",
-                    1009: "Mensagem muito grande recebida do servidor de RAG",
-                    1011: "Erro interno do servidor de RAG",
-                };
-
-                if (codeMessages[code]) {
-                    toast.info(codeMessages[code]);
-                    return;
-                }
-
-                if (!wasClean) {
-                    toast.info(`Conexão encerrada inesperadamente com o servidor de RAG: (${code})`);
-                    return;
-                }
-
-                toast.info("Conexão encerrada inesperadamente com o servidor de RAG");
-            },
-            errorCallback: (err) => {
-                toast.error(`Erro inesperado durante estabelecimento da conexão com o servidor de RAG: ${err}`);
-            }
-        });
+        if (!ws.current) return;
 
         configureHandleMessage({ 
             ws: ws.current, 
@@ -73,10 +37,11 @@ export default function Home(){
         configureHandleError({ ws: ws.current, setChatInProgress });
 
         return () => {
-            ws.current?.close();
-            ws.current = null;
+            if (ws.current) {
+                ws.current.onmessage = null;
+                ws.current.onerror = null;
+            }
         }
-
     }, [])
 
     useEffect(() => {
